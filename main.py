a0a72ceb471b132df6a023f32a443d0f93b6af5c
mamidi lokesh venkata sai <m.lokeshvenkatasai@gmail.com>
2020-09-20 21:06:28 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, url_for, session, redirect, g
+import os
+from datetime import timedelta
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    app.permanent_session_lifetime = timedelta(minutes=10)
+    app.run(debug=True)

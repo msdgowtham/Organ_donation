8dd7c716c0c70b3c1a1df37b308991a7afd4192f
Mamidi Sai Durga Gowtham <msdgowtham65@gmail.com>
2020-09-26 20:47:41 +0530

    Templates added

--- main.py
+++ main.py
@@ -4,11 +4,46 @@ from datetime import timedelta
 
 app = Flask(__name__)
 
+@app.route("/index")
 @app.route('/')
 def index():
-    print("nothing")
-    return "Hello World gowtham"
+    return render_template("index.html")
 
+@app.route('/ministries')
+def ministries():
+    return render_template("ministries.html")
+
+@app.route('/sermons')
+def sermons():
+    return render_template("sermons.html")
+
+@app.route('/event')
+def event():
+    return render_template("event.html")
+
+@app.route('/event-details')
+def event_details():
+    return render_template("event-details.html")
+
+@app.route('/about-us')
+def about_us():
+    return render_template("about-us.html")
+
+@app.route('/donation')
+def donation():
+    return render_template("donation.html")
+
+@app.route('/gallery')
+def gallery():
+    return render_template("gallery.html")
+
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+@app.route('/blog-single')
+def blog_single():
+    return render_template("blog-single.html")
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
